946d9c19af95bc3726f1db434a921e1a76742b25
GitMarusan <marusan261997@gmail.com>
2025-10-18 12:46:10 +0000

    1st saturday of java lesson

---
+++ Main.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.util.*;
+
+public class Main {
+ public static void main(String[] arg) {
+    Scanner input = new Scanner(System.in);
+    int opt;
+    int deposit;
+    int withdraw;
+    int balance;
+
+
+    User client = new User();
+    option = new opt();
+             System.out.println("========================");
+             System.out.println("=    Choose the Menu   =");
+             System.out.println("========================");
+             System.out.println("1, balance");
+             System.out.println("2, deposit");
+             System.out.println("3, withdraw");
+             System.out.println("4, exit");
+             System.out.println("Input the number");
+             input.nextInt();
+
+        switch (option) {
+            case 1:
+                
+                break;
+        
+            default:
+                break;
+        }
+
+    
+    }
+
+
+}
+
+class User {
+    String [] u_name={"a1","b1","c1"};
+    double u_acc=0.0d;
+    
+}
+    
+
+ 
+
