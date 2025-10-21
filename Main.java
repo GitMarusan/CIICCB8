b947e71c689dad9b9374ba09d746e5ca0330aea8
GitMarusan <marusan261997@gmail.com>
2025-10-21 15:54:11 +0000

    2nd day cleaning code

--- Main.java
+++ Main.java
@@ -1,47 +1,85 @@
 import java.util.*;
-import java.util.*;
+
 
 public class Main {
- public static void main(String[] arg) {
+ 
+public static <balance> void main(String[] arg) {
     Scanner input = new Scanner(System.in);
     int opt;
-    int deposit;
-    int withdraw;
-    int balance;
+    int deposit=0;
+    int withdraw=0;
+    int balance=0;
+   
 
 
-    User client = new User();
-    option = new opt();
              System.out.println("========================");
              System.out.println("=    Choose the Menu   =");
              System.out.println("========================");
+
+        do{
+            
              System.out.println("1, balance");
              System.out.println("2, deposit");
              System.out.println("3, withdraw");
              System.out.println("4, exit");
+             
              System.out.println("Input the number");
-             input.nextInt();
+             
+             opt=input.nextInt();
+            
+            
 
-        switch (option) {
+             
+        switch (opt) {
             case 1:
+                System.out.println("Your Balance is :\n"+balance);
                 
                 break;
-        
-            default:
+            case 2: 
+                System.out.println("Please input your Amount to deposit?\n");
+                 deposit=input.nextInt();
+            if (deposit >0) {
+                balance = balance + deposit;
+                System.out.println("Your Deposit amount is:" + deposit);
+            }
+                else{
+                    System.out.println("Invalid amount!!! error");
+                }
                 break;
+
+            case 3:
+                System.out.println("Please input your amount to withdraw?\n");
+                withdraw = input.nextInt();
+            if (withdraw>0) {
+                balance = balance - withdraw;
+                System.out.println("Your withdraw amount is: \n" + withdraw);
+            }
+             else{
+                    System.out.println("Invalid amount!!! error");
+                }
+                break;
+
+            case 4:
+                System.out.println("Exit");
+                break;
+
+
+            default:
+            System.out.println("Thank you! Please try again!");
+               break;
         }
-
-    
     }
+        while (opt!=4); 
+            
+        input.close();
 
-
-}
-
-class User {
-    String [] u_name={"a1","b1","c1"};
-    double u_acc=0.0d;
+    
     
 }
+}
+
+
+
     
 
  
